682f566e5c7145fd385e781a8becd5f936507d44
cuong9312 <cuong.9312@gmail.com>
2021-06-12 16:55:06 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-import { getLogger } from "log4js";
 import { logger } from "./logger";
-
-class Demo {
-  message: string;
-
-  constructor(message: string) {
-    this.message = message;
-  }
-
-  public hello() {
-    console.log(this.message);
-  }
-}
-
-const demo = new Demo('hello world');
-demo.hello();
-
 
 logger.trace("stackjava.com");
 logger.debug("stackjava.com");
